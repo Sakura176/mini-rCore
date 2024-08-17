c7f7a2201cfb4b178c9664ae7273128b31b84b50
Sakura176 <1046259179@qq.com>
2024-08-17 18:05:40 +0800

    完成格式化输出

--- os/src/main.rs
+++ os/src/main.rs
@@ -10,27 +10,20 @@ mod console;
 
 use core::{arch::global_asm, usize};
 
-use sbi::shutdown;
 global_asm!(include_str!("entry.asm"));
 
-/// clear BSS segment
+// clear BSS segment
 pub fn clear_bss() {
     extern "C" {
         fn sbss();
         fn ebss();
     }
-    (sbss as usize..ebss as usize).for_each(|a| {
-        unsafe { (a as *mut u8).write_volatile(0)}
-    });
+    (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
 }
 
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
-    // let c = 'c';
-    // console_putchar(c as usize);
     println!("hello, world!");
-    shutdown(false);
+    panic!("Shutdown machine!");
 }
-
-
